d51a39cb509f0b8fcfca0f91416d2ed6c876a0ae
Kshitiz Mhto <kshitizmhto101@gmail.com>
2025-02-08 11:09:47 +0545

    feat: added project env and asest for proect info

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -12,11 +12,14 @@ type Config struct {
 	CLI_NAME        string
 	CLI_BINARY_PATH string
 
-	FromEmail         string
-	FromEmailPassword string
-	FromEmailSMTP     string
-	SMTPAddress       string
-	OWNER_EMAIL       string
+	FromEmail              string
+	FromEmailPassword      string
+	FromEmailSMTP          string
+	SMTPAddress            string
+	OWNER_EMAIL            string
+	SUBJECT_DESC           string
+	HTML_TEMPLATE          string
+	OAUTH_CREDENTIALS_PATH string
 
 	JPEG_FORMAT string
 	JPG_FORMAT  string
@@ -29,17 +32,20 @@ var Vars = initConfig()
 func initConfig() Config {
 	godotenv.Load()
 	return Config{
-		CLI_VERSION:       GetEnv("CLI_VERSION", "1.0.0-stable"),
-		FromEmail:         GetEnv("FROM_EMAIL", ""),
-		FromEmailPassword: GetEnv("FROM_EMAIL_PASSWORD", ""),
-		FromEmailSMTP:     GetEnv("FROM_EMAIL_SMTP", "smtp.gmail.com"),
-		SMTPAddress:       GetEnv("SMTP_ADDR", "smtp.gmail.com:587"),
-		CLI_NAME:          GetEnv("CLI_NAME", "orcka"),
-		CLI_BINARY_PATH:   GetEnv("CLI_BINARY_PATH", "bin/orcka"),
-		JPEG_FORMAT:       GetEnv("JPEG_FORMAT", ".jpeg"),
-		JPG_FORMAT:        GetEnv("JPG_FORMAT", ".jpg"),
-		TXT_FORMAT:        GetEnv("TXT_FORMAT", ".txt"),
-		JSON_FORMAT:       GetEnv("JSON_FORMAT", ".json"),
+		CLI_NAME:               GetEnv("CLI_NAME", "cryptix"),
+		CLI_BINARY_PATH:        GetEnv("CLI_BINARY_PATH", "bin/cryptix"),
+		CLI_VERSION:            GetEnv("CLI_VERSION", "1.0.0-stable"),
+		FromEmail:              GetEnv("FROM_EMAIL", ""),
+		FromEmailPassword:      GetEnv("FROM_EMAIL_PASSWORD", ""),
+		FromEmailSMTP:          GetEnv("FROM_EMAIL_SMTP", "smtp.gmail.com"),
+		SMTPAddress:            GetEnv("SMTP_ADDR", "smtp.gmail.com:587"),
+		SUBJECT_DESC:           GetEnv("SUBJECT_DESC", "Hey smthg for you!!"),
+		OAUTH_CREDENTIALS_PATH: GetEnv("CREDENTIALS_PATH", ""),
+		HTML_TEMPLATE:          GetEnv("HTML_TEMPLATE", "email.html"),
+		JPEG_FORMAT:            GetEnv("JPEG_FORMAT", ".jpeg"),
+		JPG_FORMAT:             GetEnv("JPG_FORMAT", ".jpg"),
+		TXT_FORMAT:             GetEnv("TXT_FORMAT", ".txt"),
+		JSON_FORMAT:            GetEnv("JSON_FORMAT", ".json"),
 	}
 }
 
